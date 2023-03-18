f994ea16bc2397b92001cc31148f7e5f66e950d9
sorour rokni <soroorrokni@gmail.com>
2023-03-18 16:07:16 +0330

    add some assets & modify new row of content

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +13,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Profile Page',
       theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.pink.shade400,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 30, 30, 30),
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+        brightness: Brightness.dark,
+        textTheme: GoogleFonts.latoTextTheme(const TextTheme(
+            bodyMedium: TextStyle(fontSize: 15),
+            bodyLarge: TextStyle(
+                fontSize: 13, color: Color.fromARGB(200, 255, 255, 255)),
+            headlineLarge: TextStyle(fontWeight: FontWeight.bold),
+            displayLarge: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255)))),
       ),
       home: MyHomePage(),
     );
@@ -49,33 +62,72 @@ class MyHomePage extends StatelessWidget {
                   const SizedBox(
                     width: 16,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text('Brice Séraphin'),
-                      const SizedBox(
-                        height: 2,
-                      ),
-                      const Text('Product & Print Designer'),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Row(
-                        children: const [
-                          Icon(Icons.location_on_outlined),
-                          Text('Paris, France'),
-                        ],
-                      )
-                    ],
-                  )
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Brice Séraphin',
+                            style: Theme.of(context).textTheme.displayLarge),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          'Product & Print Designer',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on_outlined,
+                              size: 16,
+                              color:
+                                  Theme.of(context).textTheme.bodyLarge!.color,
+                            ),
+                            const SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              'Paris, France',
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Icon(
+                    CupertinoIcons.heart,
+                    color: Theme.of(context).primaryColor,
+                  ),
                 ],
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(32, 0, 32, 16),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 0, 32, 16),
               child: Text(
-                  'Enthusiastic young computer Geek, Freelance Designer in love of independence, I have alot of experience in graphical projects, and always give the best of myself to bring you to success.'),
+                  'Enthusiastic young computer Geek, Freelance Designer in love of independence, I have alot of experience in graphical projects, and always give the best of myself to bring you to success.',
+                  style: Theme.of(context).textTheme.bodyLarge),
             ),
+            const Divider(),
+            Row(
+              children: [
+                Text(
+                  'Skills',
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontWeight: FontWeight.w900, color: Colors.white),
+                ),
+                const SizedBox(
+                  width: 2,
+                ),
+                const Icon(
+                  CupertinoIcons.chevron_down,
+                  size: 12,
+                )
+              ],
+            )
           ],
         ));
   }
