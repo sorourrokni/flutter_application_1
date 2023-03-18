9b2e3bb6575b926ec1fd49002bb5ca75e3dd59ae
sorour rokni <soroorrokni@gmail.com>
2023-03-18 13:10:01 +0330

    set up first row of widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,15 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -28,25 +21,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget{
-
-@override
+class MyHomePage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title:Text('Curriculum Vitae'),
-        actions: [
+        appBar: AppBar(title: const Text('Curriculum Vitae'), actions: const [
           Icon(Icons.chat_bubble_outline_rounded),
           Padding(
-            padding: const EdgeInsets.fromLTRB(8, 0, 16, 0),
+            padding: EdgeInsets.fromLTRB(8, 0, 16, 0),
             child: Icon(Icons.more_vert_rounded),
           ),
         ]),
-      body:Column(children: [
-        Row(children: [
-         
-        ],)
-      ],)
-    );
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      'assets/images/profile_image.png',
+                      width: 60,
+                      height: 60,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Brice Séraphin'),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      const Text('Product & Print Designer'),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Row(
+                        children: const [
+                          Icon(Icons.location_on_outlined),
+                          Text('Paris, France'),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(32, 0, 32, 16),
+              child: Text(
+                  'Enthusiastic young computer Geek, Freelance Designer in love of independence, I have alot of experience in graphical projects, and always give the best of myself to bring you to success.'),
+            ),
+          ],
+        ));
   }
 }
